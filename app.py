89acfebc42ad0356a3bf13a411d53003229fd2ae
José Alfredo Negretti Cortés <josenegretti55@gmail.com>
2026-07-13 20:18:06 -0400

    upgrade: mejora en interfaz mobile-first, opciones premium

--- app.py
+++ app.py
@@ -416,5 +416,11 @@ def delete_lead(celular):
         return handle_error(e)
 
 
+@app.route('/reports')
+@login_required
+def reports():
+    return render_template('reports.html')
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
